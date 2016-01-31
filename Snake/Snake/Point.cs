b22566683553680f76e901fcffa9d28d94838de4
AndreyPAN <andrey.panarin@gmail.com>
2016-01-31 20:45:08 +0200

    List Collection

--- Snake/Snake/Point.cs
+++ Snake/Snake/Point.cs
@@ -8,9 +8,17 @@ namespace Snake
 {
 	class Point
 	{
-		public int x;
-		public int y;
-		public  char symb;
+		int x;
+		int y;
+		char symb;
+
+		public Point(int x, int y, char symb)
+		{
+			this.x = x;
+			this.y = y;
+			this.symb = symb;
+
+		}
 
 		public void Draw()
 
@@ -20,4 +28,4 @@ namespace Snake
 
 		}
 	}
-	}
+}
--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -13,10 +13,35 @@ namespace Snake
 			Point p1 = new Point(2, 3, '*');
 			Point p2 = new Point(5, 6, '#');
 
-
-
 			p1.Draw();
 			p2.Draw();
+
+			List<char> charList = new List<char>();
+			charList.Add('*');
+			charList.Add('#');
+			charList.Add('+');
+			charList.Add('-');
+			charList.Add('&');
+
+			foreach (var item in charList)
+			{
+				Console.WriteLine("\n"+item);
+			}
+
+			List<Point> pointCol = new List<Point>();
+			pointCol.Add(p1);
+			pointCol.Add(p2);
+			pointCol.Add(new Point(6, 7, '%'));
+			
+			foreach (var i in pointCol)
+			{
+				i.Draw();
+			} 
+
+
+
+
+
 
 
 			Console.ReadKey();
